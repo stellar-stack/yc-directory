1946f963690198e14235ca69ebceb53b4bd87d57
elliot <elliotgould5721@gmail.com>
2024-10-28 02:42:28 +0530

    created a Login and Logout functionality

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import Navbar from "../components/Navbar";
+
+export default function Layout ({ children }: Readonly<{ children : React. ReactNode}>) {
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+            
+            {children}
+        </main>
+    )
+}
