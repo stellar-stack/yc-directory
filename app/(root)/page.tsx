9a1f722cd993dd2ebf8b5e47f0dcaaee4f5582f1
elliot <elliotgould5721@gmail.com>
2024-10-28 23:28:30 +0530

    Built a hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Home</h1>
+
+    <section className="pink_container">
+
+    <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+
+    <p className="sub-heading !max-w-3xl">
+      Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+    </p>
+
+    
+    </section>
     </>
   );
 }
