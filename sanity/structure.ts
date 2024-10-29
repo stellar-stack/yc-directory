7e1145f7fb065fdaf8f7d4039099f372fb9f5f02
elliot <elliotgould5721@gmail.com>
2024-10-29 11:15:50 +0530

    created sanity schema

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,13 +3,9 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('content')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
       S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
-      ),
+      S.documentTypeListItem('startup').title('Startups'),
+      
     ])
